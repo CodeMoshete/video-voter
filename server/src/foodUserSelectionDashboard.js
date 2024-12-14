67f1c6535b1c2ff40ed5908eb9336f609b25e9ca
Benjamin Mosher <bmosher@Benjamins-MacBook-Pro.local>
2024-12-14 14:46:49 -0500

    Change theme to Bacon Christmas

--- server/src/foodUserSelectionDashboard.js
+++ server/src/foodUserSelectionDashboard.js
@@ -20,7 +20,7 @@ exports.showDashboard = async function showDashboard(serverIp, clientIp) {
   let html = fs.readFileSync(htmlPath).toString();
 
   const stylesPath =
-    path.join(global.appRoot, 'src', 'food_dashboard_content', 'food-dashboard-styles.css');
+    path.join(global.appRoot, 'src', 'food_dashboard_content', 'food-dashboard-styles-xmas.css');
   let stylesContent = fs.readFileSync(stylesPath);
   stylesContent = stylesContent.toString().split('/*SERVER-ADDRESS*/').join(serverIp);
   html = html.split('/*STYLES-CONTENT*/').join(stylesContent);
